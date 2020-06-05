c4c4876ddb7655c35b4fb7400cef8951033a157d
Sheenan Tenepre <private.sheenantenepre@gmail.com>
2020-06-05 17:46:00 +0900

    test(mailer tests): additional tests for mailers
    Mailer tests on: user welcome email and forgot password

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,3 +1,33 @@
 require 'test_helper'
 
-class UserMailerTest < ActionMailer::TestCase; end # end #   assert true # test "the truth" do
+class UserMailerTest < ActionMailer::TestCase
+  test 'should send a welcome email' do
+    user = users(:user1)
+    welcome_email = UserMailer.welcome(user)
+
+    assert_emails 1 do
+      welcome_email.deliver_now
+    end
+
+    assert_equal ['no-reply@company.com'], welcome_email.from
+    assert_equal [user.email], welcome_email.to
+    assert_equal 'Welcome to our App!', welcome_email.subject
+  end
+
+  test 'should send the forgot password information with a reset token' do
+    user = users(:user1)
+    user.generate_password_token!
+    example_url = 'http://example.com?token=xxxxx'
+
+    forgot_pass_email = UserMailer.forgot_password(user, example_url)
+
+    assert_emails 1 do
+      forgot_pass_email.deliver_now
+    end
+
+    assert_equal ['no-reply@company.com'], forgot_pass_email.from
+    assert_equal [user.email], forgot_pass_email.to
+    assert_equal 'Forgot your password?', forgot_pass_email.subject
+    assert_match example_url, forgot_pass_email.body.to_s
+  end
+end
